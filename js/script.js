4cad40bfa988443913c8627b45fcc13bc0e95412
userjhbr <126480143+userjhbr@users.noreply.github.com>
2024-07-03 12:02:49 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 
+// Basic script to alert when an achievement is unlocked
+function unlockAchievement(id) {
+    if (!localStorage.getItem(id)) {
+        localStorage.setItem(id, true);
+        alert('Achievement unlocked: ' + id);
+    }
+}
+
+// Example usage for gamification.html
+document.getElementById('3d-env-button').addEventListener('click', () => unlockAchievement('visited_3d_env'));
+document.getElementById('timeline-button').addEventListener('click', () => unlockAchievement('viewed_timeline'));
